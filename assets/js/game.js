8e2897c22fa670fc9360d8f1533be9518ab447fd
Dev.Nah <mcthundercat99@gmail.com>
2022-02-28 21:00:44 -0500

    Added start mechanics

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+//window.alert(playerName);
+// Stores info in the console
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// Announces your robot by using ("notation" + var)
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
